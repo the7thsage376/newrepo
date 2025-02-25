902c707702b74ac4455cc179bf787070c53b1491
Lerato Moloi <moloil405@gmail.com>
2025-02-25 21:39:56 +0200

    test_run

---
+++ Software_Engineering/Week_1/Day_1.py
@@ -0,0 +1,12 @@
+# program a mini-calculator using Python
+
+# Make two variables that you use for arithmetic operations(+,-,*,/)
+print("Insert two numbers.")
+num1=int(input("A: "))
+num2=int(input("B: "))
+
+#Arithmetic operations
+print(f"The sum is:{num1 + num2} ")
+print(f"The difference is:{num1 - num2} ")
+print(f"The multiplication is:{num1 * num2} ")
+print(f"The division is:{num1 / num2} ")
